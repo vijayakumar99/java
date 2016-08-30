51d1c2f9d9f2de3e2585e804906dd3dc82e2aff3
vijayakumar99 <vijaykumar43384@gmail.com>
2016-08-30 14:39:06 +0530

    vijay.java

---
+++ vijay.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class vijay
+{
+public static void main(String []args)
+{
+Scanner s=new Scanner(System.in);
+String a=s.next();
+StringBuffer sb=new StringBuffer(a);
+sb.reverse();
+System.out.println(sb);
+}
+}
